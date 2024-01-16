ff1e0ad5f2fdc91e761ce208e0e29a7c41c54cd6
km-otternauts-6758 <122175996+km-otternauts-6758@users.noreply.github.com>
2024-01-16 17:07:08 -0600

    test spark follower
    Signed-off-by: km-otternauts-6758 <122175996+km-otternauts-6758@users.noreply.github.com>

--- robot.py
+++ robot.py
@@ -1,39 +1,27 @@
 #!/usr/bin/env python3
 
 import wpilib
-import wpilib.drive
+from rev import CANSparkMax
 
 
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
-        """
-        This function is called upon program startup and
-        should be used for any initialization code.
-        """
-        self.left_motor = wpilib.Spark(0)
-        self.right_motor = wpilib.Spark(1)
-        self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
-        self.stick = wpilib.Joystick(1)
+        self.motor_one = CANSparkMax(1, CANSparkMax.MotorType.kBrushless)
+        self.motor_two = CANSparkMax(2, CANSparkMax.MotorType.kBrushless)
+        self.motor_two.follow(self.motor_one, True)
+        self.stick = wpilib.Joystick(0)
         self.timer = wpilib.Timer()
 
     def autonomousInit(self):
-        """This function is run once each time the robot enters autonomous mode."""
         self.timer.reset()
         self.timer.start()
 
     def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
-
-        # Drive for two seconds
-        if self.timer.get() < 2.0:
-            self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-        else:
-            self.drive.arcadeDrive(0, 0)  # Stop robot
+        pass
 
     def teleopPeriodic(self):
-        """This function is called periodically during operator control."""
-        self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        self.motor_one.set(self.stick.getY())
 
 
 if __name__ == "__main__":
